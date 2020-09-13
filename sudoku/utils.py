4619178e60a281178ad32b847bad5264534ebc5f
bavanya <you@example.com>
2020-09-13 15:38:54 +0530

    add logic to solve

--- sudoku/utils.py
+++ sudoku/utils.py
@@ -5,13 +5,13 @@ def cross(a, b):
     return [s+t for s in a for t in b]
 
 boxes = cross(rows, cols)
-
 row_units = [cross(r, cols) for r in rows]
 column_units = [cross(rows, c) for c in cols]
 square_units = [cross(rs, cs) for rs in ('ABC','DEF','GHI') for cs in ('123','456','789')]
 unitlist = row_units + column_units + square_units
-
 units = dict((s, [u for u in unitlist if s in u]) for s in boxes)
+peers = dict((s, set(sum(units[s],[]))-set([s])) for s in boxes)
+
 
 def display(values):
     width = 1+max(len(values[s]) for s in boxes)
@@ -24,7 +24,7 @@ def display(values):
 
 #print(boxes)
 #print(units)
-
+#print(peers)
 """
 to call display function, must convert string to dictionary: for eg, '..3.2.6..9..3.5..1..18.64....81.29..7.......8..67.82....26.95..8..2.3..9..5.1.3..' to  
 {
